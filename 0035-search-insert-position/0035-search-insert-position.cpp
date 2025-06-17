140fb85b81e398c82ba2e57295552e0c378c172a
Angel Agarwal <angel.agarwal28feb@gmail.com>
2025-06-17 17:26:51 +0530

    Time: 0 ms (100%), Space: 13.7 MB (42.3%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int c=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<target)
+            c=i+1;
+        }
+        return c;
+    }
+};
