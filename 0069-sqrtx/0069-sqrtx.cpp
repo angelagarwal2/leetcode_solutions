6a12abe5fa131fac6cc6dfa24f529ceeedb1301f
Angel Agarwal <angel.agarwal28feb@gmail.com>
2025-06-23 17:01:35 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.51%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -4,18 +4,16 @@ public:
         //return sqrt(x);
         int low=1;
         int high=x;
-        int ans=0;
         while(low<=high)
         {
             int mid=low+((high-low)/2);
-            long long sq = 1LL * mid * mid;
-            if(sq<=x) {
-                ans=max(ans,mid);
-                low=mid+1;}
+            if(mid==x/mid) return mid;
+            else if(mid>x/mid){
+                high=mid-1;}
             else{
-                high=mid-1;
+                low=mid+1;
             }
         }
-        return ans;
+        return high;
     }
 };
