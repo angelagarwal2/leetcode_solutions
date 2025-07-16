de5c14746469285759456e4d397b1f8ba024d46a
Angel Agarwal <angel.agarwal28feb@gmail.com>
2025-07-16 22:19:09 +0530

    Time: 4 ms (65.29%), Space: 96.1 MB (82.65%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        int oddCnt = 0, evenCnt = 0, cnt = 0;
-        bool flag = nums[0] % 2 == 1;
-       for (int num : nums) {
-            if (num % 2 == 1) {
-                if (flag) {
-                    flag = !flag;
-                    cnt++;
-                }
-                oddCnt++;
-            } else {
-                if (!flag) {
-                    flag = !flag;
-                    cnt++;
-                }
+        int oddCnt = 0, evenCnt = 0, cnt = 1;
+        int prev=nums[0]%2;
+        if(prev) oddCnt++;
+        else evenCnt++;
+       for (int num=1;num<nums.size();num++) {
+        int curr=nums[num]%2;
+            if (curr==0) {
                 evenCnt++;
+            } else {
+                oddCnt++;
+            }
+            if(prev!=curr){
+                cnt++;
+                prev=curr;
             }
         }
         return max({cnt, oddCnt, evenCnt});
