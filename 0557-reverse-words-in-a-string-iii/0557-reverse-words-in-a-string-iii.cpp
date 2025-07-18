9e7645152fe32d8d342e338769d5c84d69409f07
Angel Agarwal <angel.agarwal28feb@gmail.com>
2025-07-18 22:53:33 +0530

    Time: 0 ms (100%), Space: 12 MB (90.58%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stringstream ss(s);
+     /*    stringstream ss(s);
         string token="";
         string res="";
         while(ss>>token){
             reverse(begin(token),end(token));
             res+=token+" ";
         }
-        return res.substr(0,res.length()-1);
+        return res.substr(0,res.length()-1); */
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            if(s[i]!=' '){
+                int j=i;
+                while(j<n&&s[j]!=' '){
+                    j++;
+                }
+                 reverse(begin(s)+i,begin(s)+j);
+                 i=j;
+            }
+        }
+        return s;
     }
 };
