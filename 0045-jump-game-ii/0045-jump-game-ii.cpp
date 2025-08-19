f278915b7e66592ab5c68ba19d520319909898ac
Angel Agarwal <angel.agarwal28feb@gmail.com>
2025-08-19 12:52:11 +0530

    Time: 0 ms (100%), Space: 20.5 MB (62.96%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps=0;
+        int n=nums.size();
+        int r=0,l=0;
+        while(r<n-1){
+            int far=0;
+            for(int i=l;i<=r;i++)
+            far=max(far,i+nums[i]);
+            l=r+1;
+            r=far;
+            jumps++;
+        }
+        return jumps;
+    }
+};
