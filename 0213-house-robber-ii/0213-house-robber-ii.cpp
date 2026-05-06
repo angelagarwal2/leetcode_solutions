3d3e15e94b219896d36141d30dbf432d4d86bdbe
Angel Agarwal <angel.agarwal28feb@gmail.com>
2026-05-06 22:28:38 +0530

    Time: 0 ms (100%), Space: 10.2 MB (72%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+int f(vector<int>& nums){
+    int n=nums.size();
+        int prev=nums[0];
+ 	    int prev2=0;
+        for(int i=1;i<n;i++){
+            int pick=nums[i];
+            if(i>1) pick+=prev2;
+            int notpick=prev;
+            int curi=max(pick,notpick);
+	        prev2=prev;
+	        prev=curi;
+        }
+        return prev;
+}
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        vector<int> temp1,temp2;
+        for(int i=0;i<n;i++){
+            if(i!=0) temp1.push_back(nums[i]);
+            if(i!=n-1) temp2.push_back(nums[i]);
+        }
+        int ans1=f(temp1);
+        int ans2=f(temp2);
+        return max(ans1,ans2);
+    }
+};
